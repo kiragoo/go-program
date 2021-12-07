0f913d5a5ce8ebaff7d3a311b5c2efadd3fe41af
kiragoo <zhangl910201@gmail.com>
2021-12-07 20:55:13 +0800

    update go lc

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	var ans []int
+	inner(nums, 5, ans)
+	fmt.Println(ans)
+}
+
+func inner(nums []int, i int, array []int) {
+	if i == 0 {
+		_ = append(array, nums[0])
+		return
+	}
+	inner(nums, i-1, array)
+	_ = append(array, nums[i-1])
 }
